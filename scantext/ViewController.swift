f13fe074dcb7377d684f937541bc556c64f5bcbd
adilanwer <adilanwer29@gmail.com>
2022-03-31 20:03:37 +0500

    Initial Commit

---
+++ scantext/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scantext
+//
+//  Created by MacBook on 31/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
